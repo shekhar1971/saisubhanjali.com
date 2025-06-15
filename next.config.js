00a002351105097c168aec6bfabe25299cc4d94e
shekhar1971 <shekhar72@hotmail.com>
2025-06-15 15:29:36 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+// -------------------------
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true
 };
+module.exports = nextConfig;
+
+// -------------------------
