82e3a7adc3e2861638ed9bd7543d29aa22a6cf45
shekhar1971 <shekhar72@hotmail.com>
2025-06-21 16:03:51 -0400

    Create page.tsx

---
+++ app/bhajans/page.tsx
@@ -0,0 +1,10 @@
+import CD1Display from "../../components/CD1Display";
+
+export default function CDPage() {
+  return (
+    <main className="min-h-screen bg-gray-50 py-10 px-4">
+      <CD1Display />
+    </main>
+  );
+}
+ 
