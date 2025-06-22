df0a3198b3ed47b79f77acd01bc97a45722f7bf0
shekhar1971 <shekhar72@hotmail.com>
2025-06-21 22:18:41 -0400

    Update page.tsx

--- app/bhajans/page.tsx
+++ app/bhajans/page.tsx
@@ -1,11 +1,13 @@
 import CD1Display from "../../components/CD1Display";
 import CD2Display from "../../components/CD2Display";
+import CD3Display from "../../components/CD3Display";
 
 export default function BhajanPage() {
   return (
     <main className="min-h-screen bg-gray-50 py-10 px-4">
       <CD1Display />
       <CD2Display />
+      <CD3Display />
     </main>
   );
 }
