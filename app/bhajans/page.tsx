bdcad3e866f8f390f8c03388ce9207f218221e11
shekhar1971 <shekhar72@hotmail.com>
2025-06-21 22:25:38 -0400

    Update page.tsx

--- app/bhajans/page.tsx
+++ app/bhajans/page.tsx
@@ -2,6 +2,7 @@ import CD1Display from "../../components/CD1Display";
 import CD2Display from "../../components/CD2Display";
 import CD3Display from "../../components/CD3Display";
 import CD4Display from "../../components/CD4Display";
+import CD5Display from "../../components/CD5Display";
 
 export default function BhajanPage() {
   return (
@@ -10,6 +11,7 @@ export default function BhajanPage() {
       <CD2Display />
       <CD3Display />
       <CD4Display />
+      <CD5Display />
     </main>
   );
 }
