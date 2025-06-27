a2a81bdacf93f498e7cd5531990d5ed8180bb9cb
shekhar1971 <shekhar72@hotmail.com>
2025-06-27 18:29:50 -0400

    Create CDsData.ts

---
+++ components/CDsData.ts
@@ -0,0 +1,69 @@
+export const baseURL =
+  "https://juotvbuuyiisjjviqofc.supabase.co/storage/v1/object/public/sai-subhanjali-audio/";
+
+export const CDs = [
+  {
+    cdNumber: "CD1",
+    songs: [
+      {
+        file: "01 Track 1-songname.mp3",
+        title: "Song Title 1",
+      },
+      {
+        file: "01 Track 2-songname.mp3",
+        title: "Song Title 2",
+      },
+      // Add other CD1 songs here
+    ],
+  },
+  {
+    cdNumber: "CD2",
+    songs: [
+      {
+        file: "02 Track 1-Sai Nadhuni ki Vandanamu.mp3",
+        title: "Sai Nadhuni ki Vandanamu",
+      },
+      {
+        file: "02 Track 2-Sai Pada Seva.mp3",
+        title: "Sai Pada Seva",
+      },
+      {
+        file: "02 Track 3-Shirdi Sai.mp3",
+        title: "Shirdi Sai",
+      },
+      {
+        file: "02 Track 4-Dwarakamayini.mp3",
+        title: "Dwarakamayini",
+      },
+      {
+        file: "02 Track 5-Poovulu Koyandi.mp3",
+        title: "Poovulu Koyandi",
+      },
+      {
+        file: "02 Track 6-Sai Namamu.mp3",
+        title: "Sai Namamu",
+      },
+      {
+        file: "02 Track 7-Aadiyantaalu.mp3",
+        title: "Aadiyantaalu",
+      },
+      {
+        file: "02 Track 8-Kripa Drishtini.mp3",
+        title: "Kripa Drishtini",
+      },
+      {
+        file: "02 Track 9-Guruvarameeroju.mp3",
+        title: "Guruvarameeroju",
+      },
+      {
+        file: "02 Track 10-Raksha Raksha.mp3",
+        title: "Raksha Raksha",
+      },
+      {
+        file: "02 Track 11-Sai Nadhuni ki Managalam.mp3",
+        title: "Sai Nadhuni ki Managalam",
+      },
+    ],
+  },
+  // Repeat for CD3, CD4, CD5, CD6 following the same structure
+];
