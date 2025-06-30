378ae78528015ef99a46bf4523a8ec62cc524f63
shekhar1971 <shekhar72@hotmail.com>
2025-06-29 21:23:06 -0400

    Create next-sitemap.config.mjs

---
+++ next-sitemap.config.mjs
@@ -0,0 +1,12 @@
+export default {
+  siteUrl: 'https://www.saisubhanjali.com',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' },
+      { userAgent: '*', disallow: '/api/*' }, // optional
+    ],
+  },
+};
